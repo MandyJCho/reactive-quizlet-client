0d5a41c0dd9172db50b8057382244c6066441000
Mandy <chomandy96@gmail.com>
2017-07-13 22:48:30 -0500

    add material ui header

--- src/containers/StudySetsContainer.jsx
+++ src/containers/StudySetsContainer.jsx
@@ -1,29 +1,8 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import uuid from 'uuid/v1';
-
-const propTypes = {
-
-};
-
-const defaultProps = {
-
-};
 
 export default class StudySetsContainer extends Component {
   constructor(props) {
     super(props);
-
-    // State currently pulls from a static bank of data
-    this.state = {
-        [set  + uuid()] : {
-          name: 'Biology Exam',
-          cards: {
-            'mitochondria' : 'powerhouse of the cell',
-            'chitin': 'cell of wall fungi'
-          }
-        },
-    };
   }
 
   render() {
@@ -35,6 +14,3 @@ export default class StudySetsContainer extends Component {
   }
 
 }
-
-StudySetsContainer.propTypes = propTypes;
-StudySetsContainer.defaultProps = defaultProps;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Test() {
-  return (
-    <div>Hello</div>
-  );
-}
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+
+import StudySetsContainer from "./containers/StudySetsContainer";
 
 const mount = document.getElementById('root');
 
-ReactDOM.render(<Test />, mount, () => alert('Mounted'));
+function Quizlet() {
+  return (
+    <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)} >
+      <AppBar title="Reactive Quizlet" showMenuIconButton={false} />
+      <StudySetsContainer/>
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(<Quizlet />, mount);
