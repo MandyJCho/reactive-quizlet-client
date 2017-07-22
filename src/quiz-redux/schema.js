c74dde470ea634827bd5b3680d2cfbf9d024ca7a
MandyJCho <chomandy96@gmail.com>
2017-07-22 01:42:55 -0500

    modify idAttribute and handlers

--- src/quiz-redux/schema.js
+++ src/quiz-redux/schema.js
@@ -1,15 +1,16 @@
-import {normalize, schema} from 'normalizr';
-import {}
+import { schema, omit } from 'normalizr';
 
 // Create schema for user object
-export const userSchema = schema.Entity('user', {});
+export const userSchema = schema.Entity('user',
+  { idAttribute: 'userToken' },
+  thisUser => thisUser || omit(thisUser, 'userToken'),
+);
 
 // Create schema for card object
 export const cardSchema = schema.Entity('card');
-
 
 // Create a relational relationship schema for sets
 export const setSchema = schema.Entity('set', {
   user: userSchema,
   cards: [cardSchema],
-});
+}, { idAttribute: 'setID' });
