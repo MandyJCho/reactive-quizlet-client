3267f29046d08de7512cab3021c0f248ddb7d19c
MandyJCho <chomandy96@gmail.com>
2017-07-27 12:56:53 -0500

    modify set to have reudx-actions

--- src/actions/setActions.js
+++ src/actions/setActions.js
@@ -1,10 +1,27 @@
+import { createActions } from 'redux-action';
+
 import SET from './ActionTypes';
+
+export const setActionCreators = createActions({
+  SET: {
+    [SET.CREATE_SET]: [
+      title => title,
+      // META
+    ],
+    [SET.UPDATE_SET]: [
+      (id, newTitle) => {id, newTitle},
+    ],
+    [SET.DELETE_SET]: [
+
+    ],
+  },
+});
 
 // Action Creators
 export function createSetAC(title) {
   return {
     type: SET.CREATE_SET,
-    title
+    title,
   };
 }
 
@@ -13,7 +30,7 @@ export function updateSetAC(id, newTitle) {
     type: SET.UPDATE_SET,
     id,
     newTitle,
-  };c
+  };
 }
 
 export function deleteSetAC(id) {
