13555f01496b6d05890fe626d4c559db4fd81525
MandyJCho <chomandy96@gmail.com>
2017-09-13 22:29:27 -0500

    start adding new routes

--- src/components/routes/SetRoutes.jsx
+++ src/components/routes/SetRoutes.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SetsContainer from '../../containers/SetsContainer';
-import SetComponent from '../SetComponent';
+import CardsContainer from '../../containers/CardsContainer';
 
 export default function SetRoutes() {
   return (
     <div>
       <Switch>
         <Route exact path="/:id/" component={SetsContainer} />
-        <Route path="/id:/:name" component={SetComponent} />
+        <Route path="/id:/:name" component={CardsContainer} />
       </Switch>
     </div>
   );
 }
+
+// IMPORTANT FIX - Have link go to a component of routes on the sets
+// container instead of set components
