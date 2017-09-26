a896b939ff1097afe5720390a8f8187c64c2318b
Mandy <chomandy96@gmail.com>
2017-09-25 21:30:28 -0500

    adjust file organization

--- src/components/SetComponent.jsx
+++ src/components/SetComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import CardsContainer from '../containers/CardsContainer';
+import CardsContainer from '../containers/CardsContainer';  
 
 const propTypes = {
   studySet: PropTypes.shape({
