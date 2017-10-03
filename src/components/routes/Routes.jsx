e3ec11a29139e5a4992987c992b08beb46ce6d88
Mandy <chomandy96@gmail.com>
2017-10-03 00:51:23 -0500

    include graphi component

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -8,6 +8,7 @@ import {
 import HomeComponent from '../HomeComponent';
 // import SetRoutes from './SetRoutes';
 import SetComponent from '../SetComponent';
+import Graphi from '../../server/Graphi';
 
 export default function Routes() {
   return (
@@ -15,6 +16,7 @@ export default function Routes() {
       <div>
         <Route exact path="/" component={HomeComponent} />
         <Route path="/:id/:routes" component={SetComponent} />
+        <Route path='/gql' component={Graphi} />
       </div>
     </Router>
   );
---
+++ src/server/Graphi.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import GraphiQL from 'graphiql';
+
+export default React.createElement(GraphiQL);
