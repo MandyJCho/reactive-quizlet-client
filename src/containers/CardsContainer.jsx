5d143a7db115b28c00dd4c5f69526bafe39e49d8
Mandy <chomandy96@gmail.com>
2017-10-30 22:32:02 -0500

    include list rendering from r16

--- src/containers/CardsContainer.jsx
+++ src/containers/CardsContainer.jsx
@@ -5,6 +5,7 @@ import ButtonComponent from '../components/ButtonComponent';
 export default class CardsContainer extends React.Component {
   handleClick = () => ('hey');
 
+  // handle an array of components
   render() {
     return (
       <div>
